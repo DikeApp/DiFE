6c38f3ea93eb0e5226da4f9e82a148b638815273
baonguyen2604 <quocbaonguyencong@gmail.com>
2017-11-17 13:34:16 -0600

    move styles to seperate folder

--- app/containers/CreateTrip.js
+++ app/containers/CreateTrip.js
@@ -10,6 +10,8 @@ import {
 } from 'react-native'
 import { Actions } from 'react-native-router-flux';
 
+import CreateTripStyles from '../styles/createTrip_styles'
+
 class CreateTrip extends Component {
 
   constructor(props) {
@@ -33,17 +35,17 @@ class CreateTrip extends Component {
     let button = require('../../assets/create_trip/submit.png')
     return (
       <View style = {{backgroundColor: 'white', flex: 10, alignItems: 'center', justifyContent: 'space-between'}}>
-        <Image source = {header} style = {styles.header} resizeMode = 'contain' />
+        <Image source = {header} style = {CreateTripStyles.header} resizeMode = 'contain' />
 
         <TextInput
-          style = {styles.picker}
+          style = {CreateTripStyles.picker}
           placeholder = 'Enter your start point'
           onChangeText = {(origin) => this.setState({origin})}
           value = {this.state.origin}
         />
 
         <TextInput
-          style = {styles.picker}
+          style = {CreateTripStyles.picker}
           placeholder = 'Enter your destination'
           onChangeText = {(destination) => this.setState({destination})}
           value = {this.state.destination}
@@ -51,7 +53,7 @@ class CreateTrip extends Component {
 
         {/*change to date picker*/}
         <TextInput
-          style = {styles.picker}
+          style = {CreateTripStyles.picker}
           placeholder = 'Choose your departure date'
           onChangeText = {(date) => this.setState({date})}
           value = {this.state.date}
@@ -59,21 +61,21 @@ class CreateTrip extends Component {
 
         {/*change to time picker*/}
         <TextInput
-          style = {styles.picker}
+          style = {CreateTripStyles.picker}
           placeholder = 'Choose your time'
           onChangeText = {(time) => this.setState({time})}
           value = {this.state.time}
         />
 
         <TextInput
-          style = {styles.picker}
+          style = {CreateTripStyles.picker}
           placeholder = 'Share price'
           onChangeText = {(sharePrice) => this.setState({sharePrice})}
           value = {this.state.sharePrice}
         />
 
         <TextInput
-          style = {styles.note}
+          style = {CreateTripStyles.note}
           placeholder = 'Additional notes for your host'
           onChangeText = {(note) => this.setState({note})}
           value = {this.state.note}
@@ -88,30 +90,5 @@ class CreateTrip extends Component {
       )
     };
 }
-
-styles = StyleSheet.create({
-  header: {
-    flex: 3,
-    marginBottom: 5,
-    marginRight: 140
-  },
-  picker: {
-    flex: 1,
-    width: '95%',
-    height: 40,
-    borderColor: 'gray',
-    borderRadius: 10,
-    borderWidth: 1,
-    marginBottom: 15
-  },
-  note: {
-    flex: 2.5,
-    width: '95%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 10,
-    marginBottom: 10
-  }
-})
 
 export default CreateTrip;
