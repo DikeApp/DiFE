eba1bd996f237ebbd4bfc6e18a0eb941d0ca9cbe
tnlong1997 <tnlong1997@gmail.com>
2017-11-23 03:58:16 -0800

    Fix conflicts

--- app/containers/CreateTrip.js
+++ app/containers/CreateTrip.js
@@ -37,65 +37,20 @@ class CreateTrip extends Component {
     let button = require('../../assets/create_trip/submit.png')
     return (
       <View style = {{backgroundColor: 'white', flex: 10, alignItems: 'center', justifyContent: 'space-between'}}>
-<<<<<<< HEAD
-        <Image source = {header} style = {Styles.header} resizeMode = 'contain' />
-
-        <TextInput
-          style = {Styles.picker}
-          placeholder = 'Enter your start point'
-=======
         <Image source = {header} style = {BaseStyles.header} resizeMode = 'contain' />
 
         <PickerBox
           prompt = 'From'
           placeholderText = 'Enter your start point'
->>>>>>> 821e748bf4626694175a48f61dd0d418dc035fa7
           onChangeText = {(origin) => this.setState({origin})}
         />
 
-<<<<<<< HEAD
-        <TextInput
-          style = {Styles.picker}
-          placeholder = 'Enter your destination'
-=======
         <PickerBox
           prompt = 'To'
           placeholderText = 'Enter your destination'
->>>>>>> 821e748bf4626694175a48f61dd0d418dc035fa7
           onChangeText = {(destination) => this.setState({destination})}
         />
 
-<<<<<<< HEAD
-        {/*change to date picker*/}
-        <TextInput
-          style = {Styles.picker}
-          placeholder = 'Choose your departure date'
-          onChangeText = {(date) => this.setState({date})}
-          value = {this.state.date}
-        />
-
-        {/*change to time picker*/}
-        <TextInput
-          style = {Styles.picker}
-          placeholder = 'Choose your time'
-          onChangeText = {(time) => this.setState({time})}
-          value = {this.state.time}
-        />
-
-        <TextInput
-          style = {Styles.picker}
-          placeholder = 'Share price'
-          onChangeText = {(sharePrice) => this.setState({sharePrice})}
-          value = {this.state.sharePrice}
-        />
-
-        <TextInput
-          style = {Styles.note}
-          placeholder = 'Additional notes for your host'
-          onChangeText = {(note) => this.setState({note})}
-          value = {this.state.note}
-        />
-=======
         <View style = {BaseStyles.pickerContainer}>
           <Text style = {BaseStyles.pickerPrompt}>Date</Text>
           <DatePicker
@@ -151,7 +106,6 @@ class CreateTrip extends Component {
             multiline = {true}
           />
         </View>
->>>>>>> 821e748bf4626694175a48f61dd0d418dc035fa7
 
         <TouchableOpacity onPress = {this._submitCreateTrip}>
           <Image source = {button}/>
